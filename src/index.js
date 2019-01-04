b2ae0b410a1db7cd8a208058ec06bc26a31ac7dd
TessACraig89 <tesscraig.webdev@gmail.com>
2019-01-04 12:50:11 -0600

    refrences JS object in jsx using {objectName.text}

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText() {
-    return 'click on me';
-}
-
 // create a react component
 const App = () => {
+  const buttonText = { text: 'CLick me'};
   return (
     <div>
       <label className="label" for="name">Enter Name:</label>
       <input id="name" type="text"/>
       <button style={{ backgroundColor: 'blue', color: 'white'}}>
-        {getButtonText()}
+        {buttonText.text}
       </button>
     </div>
   );
