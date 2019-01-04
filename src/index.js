593441112b32943354671d1a6f5baa904c706355
TessACraig89 <tesscraig.webdev@gmail.com>
2019-01-03 21:15:44 -0600

    replaces jsx with html

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import ReactDOM from 'react-dom';
 
 // create a react component
 const App = () => {
-  return <div>Hi There</div>;
+  return (
+    <div>
+      <label class="label" for="name">Enter Name:</label>
+      <input id="name" type="text"/>
+      <button style="background-color: blue; color: white;">Submit</button>
+    </div>
+  );  
 };
 
 //  take the react component and show it on the screen
