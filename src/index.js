110bf1a2aa7a0bac8197c5879b67fa7c327317cd
TessACraig89 <tesscraig.webdev@gmail.com>
2019-01-04 12:17:15 -0600

    S2L18 refrences JS function in JSX using {functionName()}

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'click on me';
+}
+
 // create a react component
 const App = () => {
-  const buttonText = 'click me';
   return (
     <div>
       <label className="label" for="name">Enter Name:</label>
       <input id="name" type="text"/>
       <button style={{ backgroundColor: 'blue', color: 'white'}}>
-        {buttonText}
+        {getButtonText()}
       </button>
     </div>
   );
